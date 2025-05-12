43f90810c70dabcf5c6c9e2b00ac2be95156fa58
Quinten De Swaef <quintendeswaef@gmail.com>
2025-05-12 15:05:58 +0200

    Add initial setup for data aggregator service
    Introduced a new Go module and a main entry point for the data aggregator service. The service currently contains a placeholder implementation with a basic "hello" output. Future commits will expand on the functionality.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qkyrie/decentrifi/services/data-aggregator
+
+go 1.24.3
---
+++ services/data-aggregator/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is the entry point for the data aggregator service.
+	// The implementation will be added later.
+	fmt.Print("hello")
+}
